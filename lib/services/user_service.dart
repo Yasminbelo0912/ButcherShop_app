110299fba035d61cdd9bce01fa222385d69cf05e
yasmin belo <yasmin.belo@flypay.com.br>
2025-04-09 14:51:59 -0300

    First commit

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,1 +1,61 @@
+import 'dart:convert';
 
+import 'package:acougue_app/models/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserService {
+  List<User> _users = [];
+
+  Future<void> saveData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final usersListJson = _users.map((user) => user.toJson()).toList();
+    prefs.setString('users', jsonEncode(usersListJson));
+  }
+
+  Future<void> loadData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? jsonString = prefs.getString('users');
+
+    if (jsonString != null) {
+      final List<dynamic> decodeList = jsonDecode(jsonString);
+      _users = decodeList.map((item) => User.fromJson(item)).toList();
+    }
+  }
+
+  void toAdd(User user) {
+    _users.add(user);
+    saveData();
+  }
+
+  List<User> get users => _users;
+
+  User? authenticate(String email, String password) {
+    try {
+      return _users.firstWhere(
+        (user) => user.email == email && user.password == password,
+      );
+    } catch (_) {
+      return null;
+    }
+  }
+
+  void removeUser(String id) {
+    _users.removeWhere((user) => user.id == id);
+
+    saveData();
+  }
+
+  Future<void> updateUser(User user) async {
+    final index = _users.indexWhere((u) => u.id == user.id);
+    if (index != -1) {
+      _users[index] = user;
+
+      await saveData();
+    }
+  }
+
+  bool userExists(String email) {
+    final exists = _users.any((user) => user.email == email);
+    return exists;
+  }
+}
